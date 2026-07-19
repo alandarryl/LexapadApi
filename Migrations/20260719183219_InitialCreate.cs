455afa5055668f78941588b4b065492be7005667
alandarryl <okanajonathan2@gmail.com>
2026-07-19 20:41:01 +0200

    feat(database): connect API to Supabase and execute initial EF Core migration

---
+++ Migrations/20260719183219_InitialCreate.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LexapadAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Notes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Title = table.Column<string>(type: "text", nullable: false),
+                    Content = table.Column<string>(type: "text", nullable: false),
+                    FontName = table.Column<string>(type: "text", nullable: false),
+                    FontSize = table.Column<double>(type: "double precision", nullable: false),
+                    LetterSpacing = table.Column<double>(type: "double precision", nullable: false),
+                    LineHeight = table.Column<double>(type: "double precision", nullable: false),
+                    CreateAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdateAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UserId = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Notes", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Notes");
+        }
+    }
+}
